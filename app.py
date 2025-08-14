fcde06ed16312a1cc5fa41615abc26394fa73dbe
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-08-14 22:35:39 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.post("/post")
-def hello_world():
-    return "hello World"
-
-
 @app.get("/")
+def hello_world():
+    return render_template('home.html')
+
+
+@app.post("/post")
 def get():
     return "Hello World"
 
 
 if __name__ == "__main__":
-    print("I am inside")
     app.run(host='0.0.0.0', port=6000, debug=True)
